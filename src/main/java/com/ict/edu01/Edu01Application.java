cc7f7fd533b5b896012c08cd80bde4bc087a8a6c
JangDabin <xlzdscs5@gmail.com>
2025-05-27 12:36:20 +0900

    처음 코드

---
+++ src/main/java/com/ict/edu01/Edu01Application.java
@@ -0,0 +1,13 @@
+package com.ict.edu01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Edu01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Edu01Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ict/edu01/Edu01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ict.edu01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Edu01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
